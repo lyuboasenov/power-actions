3c11cb112c10a76318bb0d9075b4ddb58a9cdfb3
lyuboasenov <lyubo.asenov@gmail.com>
2023-06-22 16:37:01 +0300

    Update Remove-Snapshots.ps1

--- Remove-Snapshots.ps1
+++ Remove-Snapshots.ps1
@@ -1,1 +1,14 @@
+param(
+  [VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost] $location,
+  [string] $snapshotName
+)
 
+$vm = $null
+if ($location) {
+  $vm = Get-VM -Location $vm
+} else {
+  $vm = Get-VM
+}
+
+$snapstatus = $vm | Remove-Snapshot -confirm:$false -runasync:$true
+$snapstatus | Wait-Task
